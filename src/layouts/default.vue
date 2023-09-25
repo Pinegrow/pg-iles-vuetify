af4bbea7b74dad2a179dc0edcf2399126b2dec9f
Akayy <techakayy@gmail.com>
2023-09-25 11:58:34 +1000

    Meta updates

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,7 +5,7 @@
 
 <template>
   <div>
-    <MetaTags client:load />
+    <MetaTags />
     <div class="d-flex flex-column bg-background text-on-background">
       <!-- <NavBar class="position-fixed bg-background shadow" style="z-index: 20;" /> -->
       <!-- <div class="mt-24 shadow"> -->
