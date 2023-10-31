a793670e49d3b0c59bad12817ef467fbc300a293
Akayy <techakayy@gmail.com>
2023-10-31 21:11:34 +1100

    Centered

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -19,11 +19,40 @@
 </script>
 
 <template>
-  <v-app>
-    <v-main>
-      <span>A Fresh Start!</span>
-    </v-main>
-  </v-app>
+  <div>
+    <!-- <div class="container mx-auto"> -->
+    <v-app
+      class="bg-background text-on-background"
+      style="min-height: 100vh !important"
+    >
+      <!-- <v-app :theme="isDark ? 'dark' : 'light'"> -->
+      <!-- https://vuetifyjs.com/en/features/application-layout/
+        Navbar contains all nav components such as v-app-bar, v-system-bar, v-navigation-drawer, v-bottom-navigation -->
+      <!-- <NavBar /> -->
+      <v-main class="py-4">
+        <div>
+          <div
+            style="
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              justify-content: center;
+              padding-top: 1.5rem /* 24px */;
+              padding-bottom: 1.5rem /* 24px */;
+            "
+          >
+            <DarkModeSwitch /><span style="margin-left: 0.5rem; /* 8px */"
+              >A Fresh Start!</span
+            >
+          </div>
+        </div>
+      </v-main>
+      <!-- https://vuetifyjs.com/en/features/application-layout/
+        TheFooter contains the v-footer -->
+      <!-- <TheFooter /> -->
+    </v-app>
+    <!-- </div> -->
+  </div>
 </template>
 
 <style></style>
