511a0384bc1d0e1e854a18e826b3c3d91deb36d5
Akayy <techakayy@gmail.com>
2023-10-31 21:15:00 +1100

    Minor updates

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,29 +1,33 @@
 <script setup lang="ts">
-  /* Vuetify useTheme will not work with SSR enabled, more info - https://vuetify-nuxt-module.netlify.app/guide/server-side-rendering.html#vuetify-themes */
-  // import { useCustomTheme } from '~/composables/custom-theme'
-  // const { isDark } = useCustomTheme()
-
-  /* Update on using the above technique - We now use SSR HTTP Client hints technique instead of the above - https://vuetify-nuxt-module.netlify.app/guide/server-side-rendering.html#ssr-http-client-hints */
-
   import { getCurrentInstance } from 'vue'
   import vuetify from '@/plugins/vuetify'
 
-  import { pg_background_urls } from '~~/themes/pg-vuetify/tokens.mjs'
-
-  const heroImageUrl =
-    pg_background_urls['design-image-large'] ||
-    pg_background_urls['design-image']
+  import { heroImageUrl } from '@/utils/hero'
 
   const app = getCurrentInstance().appContext.app
   app.use(vuetify)
 </script>
 
 <template>
-  <v-app>
-    <v-main>
-      <SampleCard />
-    </v-main>
-  </v-app>
+  <div>
+    <!-- <div class="container mx-auto"> -->
+    <v-app
+      class="bg-background text-on-background"
+      style="min-height: 100vh !important"
+    >
+      <!-- <v-app :theme="isDark ? 'dark' : 'light'"> -->
+      <!-- https://vuetifyjs.com/en/features/application-layout/
+        Navbar contains all nav components such as v-app-bar, v-system-bar, v-navigation-drawer, v-bottom-navigation -->
+      <!-- <NavBar /> -->
+      <v-main class="py-4">
+        <SampleCard />
+      </v-main>
+      <!-- https://vuetifyjs.com/en/features/application-layout/
+        TheFooter contains the v-footer -->
+      <!-- <TheFooter /> -->
+    </v-app>
+    <!-- </div> -->
+  </div>
 </template>
 
 <style></style>
