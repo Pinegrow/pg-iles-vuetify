4a0ea5145f28079fa7aed526b4a32e291258377b
Akayy <techakayy@gmail.com>
2024-02-01 22:10:25 +1100

    Updated gitignore & readme, added rollup visualizer

--- iles.config.ts
+++ iles.config.ts
@@ -8,6 +8,8 @@ import presetIcons from '@unocss/preset-icons'
 // import VueDevTools from 'vite-plugin-vue-devtools'
 // import myIlesModule from './src/modules/my-module'
 
+// import { visualizer } from 'rollup-plugin-visualizer'
+
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 import site from './src/site'
@@ -194,6 +196,14 @@ export default defineConfig({
     ssr: {
       noExternal: ['vuetify'],
     },
+
+    // build: {
+    //   // Vite uses Rollup under the hold, so rollup options & plugins can be used for advanced usage
+    //   rollupOptions: {
+    //     plugins: [visualizer()],
+    //   },
+    // },
+
     resolve: {
       alias: {
         /* Must be either an object, or an array of { find, replacement, customResolver } pairs. */
