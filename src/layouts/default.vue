c2ded43c3bf409dc1a84c1929ed884096256c239
Akayy <techakayy@gmail.com>
2024-06-06 18:12:56 +1000

    Initial commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,20 @@
+<script client:load lang="ts">
+  //Move to WalkThrough.vue
+  import '@/plugins/youtube.client'
+</script>
+
+<template>
+  <div>
+    <HeadAndMeta />
+    <div class="d-flex flex-column bg-background text-on-background">
+      <!-- <NavBar class="position-fixed bg-background shadow" style="z-index: 20;" /> -->
+      <!-- <div class="mt-24 shadow"> -->
+      <div>
+        <slot />
+      </div>
+      <!-- </div> -->
+      <!-- <TheFooter /> -->
+    </div>
+  </div>
+</template>
+<style></style>
