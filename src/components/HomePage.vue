f4736a412c0ffc8969a27ce2ed6a68edcff413f8
Akayy <techakayy@gmail.com>
2024-10-07 12:58:47 +1100

    Cleanup

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -31,7 +31,7 @@
               padding-bottom: 1.5rem /* 24px */;
             "
           >
-            <IslandDarkModeSwitch /><span style="margin-left: 0.5rem; /* 8px */"
+            <DarkModeSwitch /><span style="margin-left: 0.5rem; /* 8px */"
               >A Fresh Start!</span
             >
           </div>
