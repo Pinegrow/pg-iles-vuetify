884831447a190075258d1615d5db27b50d757c93
Akayy <techakayy@gmail.com>
2025-01-11 22:57:32 +1100

    Updated nav-menu composable and site meta

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,11 +3,15 @@
   import '@/plugins/youtube.client'
 </script>
 
+<script setup lang="ts">
+  useHeadAndMeta()
+  // const { route } = usePage()
+</script>
+
 <template>
   <div>
-    <HeadAndMeta />
     <div class="d-flex flex-column bg-background text-on-background">
-      <!-- <NavBar class="position-fixed bg-background shadow" style="z-index: 20;" /> -->
+      <!-- <NavBar class="position-fixed bg-background shadow" style="z-index: 20;" :current-path="route.path" /> -->
       <!-- <div class="mt-24 shadow"> -->
       <div>
         <slot />
